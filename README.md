ced80933dff04e55ecf693e0cc5bee54b23d27b5
R. W <123055089+rhwvu@users.noreply.github.com>
2024-08-01 01:43:30 -0400

    refactored code quite a bit, it is a lot cleaner and simpler now. updated comments and simplified and updated README

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
 # stock-data-puller
-Pulls stock data from Yahoo Finance with the yfinance API to be used in a Discounted Cash Flow
+Pulls stock data from Yahoo Finance with the yfinance API to be used in a Discounted Cash Flow (DCF)
 
 Features:
 - [x] Gets ticker from user
-- [X] Allow user to edit a config file to set defaults
-- [x] Pulls income statement, balance sheet, and cash flow for the company
+- [X] Allow user to edit a config file to set default settings
+- [x] Pulls income statement, balance sheet, and cash flow for the ticker's company
 - [x] Puts into a data.xlsx Excel file to be read by other Excel files (such as one for a DCF)
 - [ ] Scrape WACC and Yahoo Finance analyst numbers (currently just opens the websites to be manually grabbed)
-	- [X] Otherwise find a way to only open one finbox link (currently approximated with ticker length, can be improved)
-- [ ] Allow for more expense names as needed
-- [X] Allow for companies with no Cost of Revenue
-- [X] Need to figure out how to get "Loss Adjustment Expense" expense (ex: ELV) (fixed in yfinance library)
-- [ ] Possibly change the way companies with less than 4 years of Yahoo finances are put into Excel
-- [ ] Split off all variables into separate sheets for the best variablility
-- [ ] Deal with removing NA values for each one individually
-- [ ] Make my own DCF sheet (currently using a proprietary one)
+	- [ ] Otherwise find a way to get an exchange for ticker (exchange currently approximated with ticker length, can be improved)
+- [X] Deals with many individual companies' different expense categorizations
+- [ ] Make DCF sheet to go with the project (I'm currently using a proprietary one)
 
 With config.txt, you can set defaults and settings as the following:
 - First line is a default ticker to pull if none is inputted, (default as MSFT)
--- data.py
+++ data.py
@@ -2,11 +2,11 @@ import yfinance as yf
 import pandas as pd
 import webbrowser
 
-# Open config file and read each parameter into an array
+# Open config file and read each parameter into a list
 config_file = open("config.txt", "r")
 config = config_file.readlines()
 
-# Get ticker from user, put it to all caps, defaults to MSFT
+# Get ticker from user, put it to uppercase, defaults to config[0]
 ticker = input("Ticker: ").strip().upper() or config[0].strip().upper()
 stock = yf.Ticker(ticker)
 
@@ -15,86 +15,42 @@ income = stock.income_stmt
 balance = stock.balance_sheet
 cashflow = stock.cashflow
 
-# Get path from user, make file called data.xlsx
+# Get path from config[1], make file called data.xlsx
 path_in = config[1].strip()
 path = path_in + "data.xlsx"
 
-# Ask if user wants to open websites automatically
+# Ask if user wants to open websites automatically, or default to config[2]
 open_browser = input("Open websites? Y or N: ").strip().upper() or config[2].strip().upper()
 
 # Make ExcelWriter
 writer = pd.ExcelWriter(path)
 
-# Put the three tables from before together, and transpose
+# Put the three tables together, transpose, drop empty years, and sort by year
 result = pd.concat([income, balance, cashflow])
 resultT = result.transpose()
 resultT = resultT[resultT["Total Revenue"].notna()]
+resultT = resultT.sort_index(axis=1, ascending=True)
 
 # List of stats of interest
-stat_list = ["Total Revenue", "Reconciled Depreciation", "Capital Expenditure", "Working Capital", "Cash Cash Equivalents And Short Term Investments", "Cash Cash Equivalents And Federal Funds Sold"]
-expense_list = ["Research And Development", "Selling General And Administration", "Other Operating Expenses", "Loss Adjustment Expense", "Occupancy And Equipment", "Other Non Interest Expense", "Professional Expense And Contract Services Expense", "Other Taxes"]
-debt_list = ["Current Debt And Capital Lease Obligation", "Long Term Debt And Capital Lease Obligation"]
+stat_list = ["Total Revenue", "Cost Of Revenue", "Reconciled Depreciation", "Capital Expenditure",
+    "Working Capital", "Cash Cash Equivalents And Short Term Investments", "Cash Cash Equivalents And Federal Funds Sold",
+    "Research And Development", "Selling General And Administration", "Other Operating Expenses",
+    "Loss Adjustment Expense", "Occupancy And Equipment", "Other Income Expense", "Other Non Interest Expense",
+    "Professional Expense And Contract Services Expense", "Other Taxes", "Current Debt And Capital Lease Obligation",
+    "Long Term Debt And Capital Lease Obligation", "Special Income Charges", "Other Special Charges"]
 
-# Clean up results with the stat_list, drop empty years, then transpose again, sort by year
-clean = resultT[resultT.columns.intersection(stat_list)]
+# Clean up results with stat_list and transpose again
+clean = resultT.reindex(columns=stat_list).fillna(0)
 cleanT = clean.transpose()
-cleanT = cleanT.sort_index(axis=1, ascending=True)
 
-expenses = resultT[resultT.columns.intersection(expense_list)]
-expensesT = expenses.transpose()
-expensesT = expensesT.sort_index(axis=1, ascending=True)
+# Write the dataset to a sheet in data.xlsx
+cleanT.to_excel(writer, sheet_name = "data")
+sheet = writer.sheets["data"]
 
-debt = resultT[resultT.columns.intersection(debt_list)]
-debtT = debt.transpose()
-debtT = debtT.sort_index(axis=1, ascending=True)
-
-cogs = resultT[resultT.columns.intersection(["Cost Of Revenue"])]
-cogsT = cogs.transpose()
-cogsT = cogsT.sort_index(axis=1, ascending=True)
-
-other = resultT[resultT.columns.intersection(["Other Income Expense"])]
-otherT = other.transpose()
-otherT = otherT.sort_index(axis=1, ascending=True)
-
-special = resultT[resultT.columns.intersection(["Special Income Charges", "Other Special Charges"])]
-specialT = special.transpose()
-specialT = specialT.sort_index(axis=1, ascending=True)
-
-# Write the three separate datasets to Excel sheets
-cleanT.to_excel(writer, sheet_name = "main")
-sheet1 = writer.sheets["main"]
-
-expensesT.to_excel(writer, sheet_name = "expenses")
-sheet2 = writer.sheets["expenses"]
-
-debtT.to_excel(writer, sheet_name = "debt")
-sheet3 = writer.sheets["debt"]
-
-cogsT.to_excel(writer, sheet_name = "COGS")
-sheet4 = writer.sheets["COGS"]
-
-otherT.to_excel(writer, sheet_name = "other")
-sheet4 = writer.sheets["other"]
-
-specialT.to_excel(writer, sheet_name = "special")
-sheet5 = writer.sheets["special"]
-
-# Put everything into a currency format in case user wants to read data.xlsx directly
+# Put into a currency format in case user wants to read data.xlsx directly
 fmt_currency = writer.book.add_format({"num_format" : "$#,##0" ,"bold" :False})
-sheet1.set_column("A:A", 30)
-sheet1.set_column("B:E", 20, fmt_currency)
-
-sheet2.set_column("A:A", 30)
-sheet2.set_column("B:E", 20, fmt_currency)
-
-sheet3.set_column("A:A", 30)
-sheet3.set_column("B:E", 20, fmt_currency)
-
-sheet4.set_column("A:A", 30)
-sheet4.set_column("B:E", 20, fmt_currency)
-
-sheet5.set_column("A:A", 30)
-sheet5.set_column("B:E", 20, fmt_currency)
+sheet.set_column("A:A", 30)
+sheet.set_column("B:E", 20, fmt_currency)
 
 # Close ExcelWriter
 writer.close()
@@ -107,12 +63,12 @@ WACC_NYSE = "https://finbox.com/NYSE:" + ticker + "/models/wacc/"
 # If user wants to open websites, open them
 if open_browser == "Y":
     webbrowser.open(analysis_link)
-    # Ticker length being 4 or above is usually able to check whether a stock is in NASDAQ or NYSE, other exchanges not yet supported for my finbox links
+    # Ticker length being 4 or above is usually able to check whether a stock is in NASDAQ or NYSE
     if len(ticker) >= 4:
         webbrowser.open(WACC_NASDAQ)
     else:
         webbrowser.open(WACC_NYSE)
         
-# To allow for any errors to be read if run like an executable
+# config[3] allows for errors to be read after run complete
 if config[3].strip().upper() == "Y":
     input("...")
